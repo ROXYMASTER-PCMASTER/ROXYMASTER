28381165730aa0f26fc887939e0d5b74c936e040
ROXYMASTER <oxfordmw@gmail.com>
2026-05-15 13:25:06 -0500

    Delete pcmaster/pcmaster directory

--- pcmaster/pcmaster/_verificar_tabla.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('data/roxymaster.db')
-row = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='contextos_streamer'").fetchone()
-print(row)
-conn.close()
